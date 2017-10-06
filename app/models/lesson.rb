97fd94aa5d4d1d769c2a7ad93073a5f466b48fb7
Nic Small <nic@element84.com>
2017-10-06 02:41:28 -0400

    Fixed syntax error with has_many

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,5 @@
 class Lesson < ApplicationRecord
-  has_many: lesson_exercises
-  has_many: lessons, :through => :lesson_exercises
+  has_many :lesson_exercises
+  has_many :lessons, :through => :lesson_exercises
   belongs_to :user
 end
