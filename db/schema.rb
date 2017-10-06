e85da059191f3bf7e27458c2fec6c76f09eb8674
Nic Small <nic@element84.com>
2017-10-06 02:34:59 -0400

    Changed db from sqlite3 to psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171004150910) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "alternate_names", force: :cascade do |t|
     t.string "name"
     t.integer "exercise_id"
